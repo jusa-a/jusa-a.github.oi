40be7045ce008457b89e8ebc88f7c3e15e01c04c
jusa-a <58844123+jusa-a@users.noreply.github.com>
2020-02-24 11:43:22 +0200

    initial commit

---
+++ src/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top-0
+        }, 300, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#project-nav a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos + 200 && refElement.position().top + refElement.height() > scrollPos) {
+            $('#project-nav ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
+
+//resource:
+//https://stackoverflow.com/questions/9979827/change-active-menu-item-on-page-scroll
